c26429046a388635b14baa89391ef27ea1274d2a
katemont <katharine.montgomery@gmail.com>
2014-10-13 13:26:40 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrinkfinderApp::Application.config.secret_token = '0f26f3778490610d9947aa6a410dbb5711b547d37428554766cded4220e36111c386db3ee81f205ae421f52ce7d7ef3aee276e61fa38e0574ab5156a99906999'
