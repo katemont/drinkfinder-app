6595e20110b5afff28f5186be7f14f75375afcac
katemont <katharine.montgomery@gmail.com>
2014-10-14 21:50:06 +0100

    adds name, username, profile_pic and status to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141014135114) do
+ActiveRecord::Schema.define(:version => 20141014203751) do
 
   create_table "recommendations", :force => true do |t|
     t.text     "review"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(:version => 20141014135114) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
+    t.string   "profile_pic"
+    t.string   "username"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
