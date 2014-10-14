c9bbcc887288f9901b45dd5dd74de5d4906d04a5
katemont <katharine.montgomery@gmail.com>
2014-10-14 15:13:13 +0100

    creates recommendations model

---
+++ app/models/recommendation.rb
@@ -0,0 +1,5 @@
+class Recommendation < ActiveRecord::Base
+  attr_accessible :bar_id, :review, :user_id
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141013131214) do
+ActiveRecord::Schema.define(:version => 20141014135114) do
+
+  create_table "recommendations", :force => true do |t|
+    t.text     "review"
+    t.integer  "bar_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
