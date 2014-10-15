81bd52dc387bd4edf8cce7f69d3fc3344b479b5e
katemont <katharine.montgomery@gmail.com>
2014-10-15 14:07:54 +0100

    creates bar model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141014203751) do
+ActiveRecord::Schema.define(:version => 20141015130600) do
+
+  create_table "bars", :force => true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "recommendations", :force => true do |t|
     t.text     "review"
