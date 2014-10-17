d5bf1b09249aa2b9b8cfb98f647a4e984f082dc2
katemont <katharine.montgomery@gmail.com>
2014-10-17 15:44:47 +0100

    adds geocoder gem and creates migration to add lat and long to bar model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141015155606) do
+ActiveRecord::Schema.define(:version => 20141017144027) do
 
   create_table "bars", :force => true do |t|
     t.string   "name"
     t.string   "location"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "recommendations", :force => true do |t|
