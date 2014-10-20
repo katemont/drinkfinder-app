e0036fd2f011b803c409f210d37987d3b7c55f3f
katemont <katharine.montgomery@gmail.com>
2014-10-20 14:37:12 +0100

    autocomplete box added retrieving info from google places. whoop

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,34 +1,34 @@
 var myMap = myMap || {},
-    map = {},
-    marker = {};
-    popup = {}
-   
-   
+map = {},
+marker = {};
+popup = {}
+
+
 myMap.initialize = function() {
 
-    var mapOptions = {
-        center: { lat:  51.52, lng: -0.115},     
-        zoom: 14,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
+  var mapOptions = {
+    center: { lat:  51.52, lng: -0.115},     
+    zoom: 14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
 
-    var mapCanvas = $('#map-canvas')[0];
+  var mapCanvas = $('#map-canvas')[0];
 
-    map = new google.maps.Map(mapCanvas, mapOptions);
+  map = new google.maps.Map(mapCanvas, mapOptions);
 
-    var markerOptions = {
-      position: new google.maps.LatLng(51.53, -0.109446),
-      map: map
-    };
+  var markerOptions = {
+    position: new google.maps.LatLng(51.53, -0.109446),
+    map: map
+  };
 
-    marker = new google.maps.Marker(markerOptions);
+  marker = new google.maps.Marker(markerOptions);
 
-    popup = new google.maps.InfoWindow(infoWindowOptions);
+  popup = new google.maps.InfoWindow(infoWindowOptions);
 
-    google.maps.event.addListener(marker, 'click', function(){
-      popup.open(map, marker);
-    });
-    $('#locate').on('click', function(){
+  google.maps.event.addListener(marker, 'click', function(){
+    popup.open(map, marker);
+  });
+  $('#locate').on('click', function(){
       // console.log('click');
       $(this).text('loading...');
 
@@ -38,21 +38,41 @@ myMap.initialize = function() {
         alert("Your browser doesn't support geolocation");
       };
     });
+  var autocomplete = new google.maps.places.Autocomplete($('#autocomplete')[0]);
+  autocomplete.bindTo('bounds',map);
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function(){
+
+    popup.close(); 
+
+    var place = autocomplete.getPlace(); 
+ 
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport); 
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    };
+
+    marker.setPosition(place.geometry.location);
+
+    popup.setContent('<div>'+ place.name +'</div>');
+
+
+    popup.open(map, marker);
+
+  });
 }
 
-// setTimeout(function(){
-//   map.setCenter(marker.getPosition());
-// }, 5000);
- 
-   var infoWindowOptions = {
-   content: 'You.',
- };
+var infoWindowOptions = {
+ content: 'You.',
+};
 
-   function successCallback(position){
-   console.log(position);
+function successCallback(position){
+ console.log(position);
  
-   var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-   console.log(latlng);
+ var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ console.log(latlng);
 
    // changing position of the existing marker:
    marker.setPosition(latlng);
@@ -67,6 +87,7 @@ myMap.initialize = function() {
    console.log();
    alert('Could not retrieve your location, please try again');
  };
+
 
 
  google.maps.event.addDomListener(window, 'load', myMap.initialize);
