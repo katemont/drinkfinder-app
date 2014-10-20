cb8c2c573ed4869841acf1fc836ec523bf7ca57d
katemont <katharine.montgomery@gmail.com>
2014-10-20 13:25:26 +0100

    adds map marker

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,3 @@
-
 var myMap = myMap || {};
 
 myMap.initialize = function() {
@@ -9,8 +8,52 @@ myMap.initialize = function() {
         mapTypeId:google.maps.MapTypeId.ROADMAP
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    var mapCanvas = $('#map-canvas')[0];
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var markerOptions = {
+      position: new google.maps.LatLng(51.53, -0.109446),
+      map: map
+    };
+
+    var marker = new google.maps.Marker(markerOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', myMap.initialize);
+// setTimeout(function(){
+//   map.setCenter(marker.getPosition());
+// }, 5000);
+ 
+ $('#locate').on('click', function(){
+   console.log('click');
+   $(this).text('loading...');
+
+   if (navigator.geolocation){
+     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+   } else {
+     alert("Your browser doesn't support geolocation");
+   };
+ });
+
+ function successCallback(position){
+   console.log(position);
+ 
+   var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+   console.log(latlng);
+
+   // changing position of the existing marker:
+   marker.setPosition(latlng);
+   map.setCenter(latlng);
+   map.setZoom(17);
+   popup.setContent('You are here!');
+   popup.open(map, marker);
+   $('#locate').text('locate me');
+ };
+
+ function errorCallback(error){
+   console.log();
+   alert('Could not retrieve your location, please try again');
+ };
+
+
+ google.maps.event.addDomListener(window, 'load', myMap.initialize);
