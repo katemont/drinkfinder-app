d7090886d0d8fc39b1bb18870474cbd929722fb0
katemont <katharine.montgomery@gmail.com>
2014-10-20 14:07:54 +0100

    fixes geolocation

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,6 @@
-var myMap = myMap || {};
+var myMap = myMap || {},
+    map = {},
+    marker = {};
 
 myMap.initialize = function() {
 
@@ -10,30 +12,32 @@ myMap.initialize = function() {
 
     var mapCanvas = $('#map-canvas')[0];
 
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    map = new google.maps.Map(mapCanvas, mapOptions);
 
     var markerOptions = {
       position: new google.maps.LatLng(51.53, -0.109446),
       map: map
     };
 
-    var marker = new google.maps.Marker(markerOptions);
+    marker = new google.maps.Marker(markerOptions);
+
+    $('#locate').on('click', function(){
+      console.log('click');
+      $(this).text('loading...');
+
+      if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      } else {
+        alert("Your browser doesn't support geolocation");
+      };
+    });
 }
 
 // setTimeout(function(){
 //   map.setCenter(marker.getPosition());
 // }, 5000);
  
- $('#locate').on('click', function(){
-   console.log('click');
-   $(this).text('loading...');
-
-   if (navigator.geolocation){
-     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-   } else {
-     alert("Your browser doesn't support geolocation");
-   };
- });
+ 
 
  function successCallback(position){
    console.log(position);
@@ -44,9 +48,9 @@ myMap.initialize = function() {
    // changing position of the existing marker:
    marker.setPosition(latlng);
    map.setCenter(latlng);
-   map.setZoom(17);
-   popup.setContent('You are here!');
-   popup.open(map, marker);
+   map.setZoom(18);
+   // popup.setContent('You are here!');
+   // popup.open(map, marker);
    $('#locate').text('locate me');
  };
 
