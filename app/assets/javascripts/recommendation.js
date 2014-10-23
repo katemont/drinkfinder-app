9c1a8498a9c33df831894d25483fb85f9dc34c68
katemont <katharine.montgomery@gmail.com>
2014-10-23 20:04:29 +0100

    adds some additional styling to recommendations, failed to get animation working

--- app/assets/javascripts/recommendation.js
+++ app/assets/javascripts/recommendation.js
@@ -1,26 +1,41 @@
-// Character counter needs amending
 
 // $().ready(function(e){
 
-//   var characterCount = 0;
-//   $('#new_recommendation').before("<p id='new_recommendation_character_count'></p>");
-//   $('#new_recommendation div:last > input').attr('disabled', 'disabled');
-
-//   $('#new_recommendation').on('keyup',function(e){
-//     characterCount = $('#recommendation_content').val().length;  
-//     checkMessageLengthAndEnableAndDisableNewRecommendationBox(characterCount);
-//     displayCharacterCount(characterCount);
+//   $('.recommendations-display').on('click', 'a', function(e) {
+//     e.preventDefault();
+//     // console.log($(this).attr('href'));
+//     $('.recommendations-display').load($(this).attr('href') + ' .recommendations-display', function() {
+//       $('.recommendation').hide();
+//       $('.recommendation').each(function(i) {
+//         $(this).delay((i++) * 500).fadeIn('slow'); 
+//       });
+//     });
 //   });
+// });
 
-//   function displayCharacterCount(characterCount){
-//     $('#new_recommendation_character_count').text(characterCount + "/200");
-//   }
+// // Character counter needs amending
 
-//   function checkMessageLengthAndEnableAndDisableNewRecommendationBox(charlength) {
-//     if (charlength > 200) {
-//         $("input[type=submit]").attr('disabled', 'true');
-//     } else {
-//         $("input[type=submit]").removeAttr('disabled');
-//     }
-//   }
-//   
+// // $().ready(function(e){
+
+// //   var characterCount = 0;
+// //   $('#new_recommendation').before("<p id='new_recommendation_character_count'></p>");
+// //   $('#new_recommendation div:last > input').attr('disabled', 'disabled');
+
+// //   $('#new_recommendation').on('keyup',function(e){
+// //     characterCount = $('#recommendation_content').val().length;  
+// //     checkMessageLengthAndEnableAndDisableNewRecommendationBox(characterCount);
+// //     displayCharacterCount(characterCount);
+// //   });
+
+// //   function displayCharacterCount(characterCount){
+// //     $('#new_recommendation_character_count').text(characterCount + "/200");
+// //   }
+
+// //   function checkMessageLengthAndEnableAndDisableNewRecommendationBox(charlength) {
+// //     if (charlength > 200) {
+// //         $("input[type=submit]").attr('disabled', 'true');
+// //     } else {
+// //         $("input[type=submit]").removeAttr('disabled');
+// //     }
+// //   }
+// //   
