75ba27a72f30f7e673d3107469a917a6338ed258
katemont <katharine.montgomery@gmail.com>
2014-10-23 11:48:54 +0100

    adds logic to display div only if user has friends

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,7 +1,7 @@
 class Bar < ActiveRecord::Base
   attr_accessible :location, :name, :latitude, :longitude
 
-  has_many :recommendations
+  has_many :recommendations, :dependent => :destroy
 
   geocoded_by :location  
   after_validation :geocode
